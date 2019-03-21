8feb5f24b3b3fff3475ce31293ffa0aa12759f98
Sean Killian <skillian92@gmail.com>
2019-03-21 17:30:44 -0400

    Standalone executable source
    For wrapping text from the command line.

---
+++ textwrap/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/skillian/textwrap"
+)
+
+func main() {
+	var columns int
+	flag.IntVar(&columns, "c", 80, "Columns to wrap output")
+
+	defaultUsage := flag.Usage
+
+	flag.Usage = func() {
+		defaultUsage()
+		fmt.Println(`
+positional arguments:
+  source
+                The source file to read from (default: "-" for stdin)
+
+  target
+                The target file to write to (default: "-" for stdout)`)
+	}
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	sourceName, targetName := "", ""
+
+	switch len(args) {
+	case 2:
+		targetName = args[1]
+		fallthrough
+	case 1:
+		sourceName = args[0]
+	default:
+		log.Fatal("expected source and target filenames")
+	}
+
+	var err error
+
+	var source io.ReadCloser
+	if sourceName == "" || sourceName == "-" {
+		source = ioutil.NopCloser(os.Stdin)
+	} else {
+		source, err = os.Open(sourceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	defer logClose(source)
+
+	var target io.WriteCloser
+	if targetName == "" || targetName == "-" {
+		target = &nopWriteCloser{os.Stdout}
+	} else {
+		target, err = os.Create(sourceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	defer logClose(target)
+
+	source, err = textwrap.NewReader(source, columns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err = io.Copy(target, source); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getFile(name string, opener func(name string) (*os.File, error), fallback *os.File) (io.ReadCloser, error) {
+	if name == "" || name == "-" {
+		return fallback, nil
+	}
+	return opener(name)
+}
+
+func logClose(c io.Closer) {
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type nopWriteCloser struct{ io.Writer }
+
+func (wc *nopWriteCloser) Close() error { return nil }
